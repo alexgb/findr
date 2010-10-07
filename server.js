146eaf848722c0b4e9429591b2a9a4cd9065b356
Alex Gibbons <alex.gibbons@gmail.com>
2010-10-06 21:56:28 -0700

    removed stray code in server

--- server.js
+++ server.js
@@ -108,22 +108,6 @@ var onlocationNotification = function(client, message) {
 
 io = sockets.listen(app);
 
-io.on('clientConnect', function(client){
- // client.send({ buffer: buffer });
- // client.broadcast({ announcement: client.sessionId + ' connected' });
- // 
- // client.on('message', function(message){
- //   var msg = { message: [client.sessionId, message] };
- //   buffer.push(msg);
- //   if (buffer.length > 15) buffer.shift();
- //   client.broadcast(msg);
- // });
- // 
- // client.on('disconnect', function(){
- //   client.broadcast({ announcement: client.sessionId + ' disconnected' });
- // });
-});
-
 
 io.on('clientMessage', function(message, client) {
   switch(message.type) {
